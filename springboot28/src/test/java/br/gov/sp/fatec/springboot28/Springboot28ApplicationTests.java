a51e731317402ada50a6aabd584f69ee5c791a3e
Ana Luisa <127700485+LuisaAndrade28@users.noreply.github.com>
2025-09-14 19:54:14 -0300

    spring start

---
+++ springboot28/src/test/java/br/gov/sp/fatec/springboot28/Springboot28ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.springboot28;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot28ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
